6207f30632c52769190d5df94aff8545380405f1
pil97 <seungpil97@naver.com>
2026-02-24 09:45:18 +0900

    chore: initial commit

---
+++ src/main/java/com/pil97/ticketing/TicketingReservationApplication.java
@@ -0,0 +1,13 @@
+package com.pil97.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingReservationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TicketingReservationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/pil97/ticketing/TicketingReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pil97.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingReservationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
